fbfa386089021b899f1c2551a594d067614d44b1
whichxjy <whichxjy@gmail.com>
2020-02-22 15:22:44 +0800

    Update

---
+++ Ch_02_Linked_Lists/Q2_01_Remove_Dups/main_v2.cpp
@@ -0,0 +1,114 @@
+// 时间复杂度：O(N ^ 2)
+// 空间复杂度：O(1)
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <cassert>
+
+template <typename T>
+struct Node {
+    T value;
+    std::shared_ptr<Node> next;
+
+    Node() = default;
+    Node(T value) : value(value), next(nullptr) {}
+    Node(T value, std::shared_ptr<Node> next) : value(value), next(next) {}
+};
+
+template <typename T>
+struct List {
+    std::shared_ptr<Node<T>> head;
+
+    List() = default;
+    List(std::shared_ptr<Node<T>> head) : head(head) {}
+
+    void append(T value) {
+        if (head == nullptr) {
+            head = std::make_shared<Node<T>>(Node(value));
+        }
+        else {
+            std::shared_ptr<Node<T>> curr = head;
+            while (curr->next != nullptr) {
+                curr = curr->next;
+            }
+            curr->next = std::make_shared<Node<T>>(value);
+        }
+    }
+
+    void remove_dups() {
+        std::shared_ptr<Node<T>> curr = head;
+
+        while (curr != nullptr) {
+            std::shared_ptr<Node<T>> runner = curr;
+
+            while (runner->next != nullptr) {
+                if (runner->next->value == curr->value) {
+                    // remove
+                    runner->next = runner->next->next;
+                }
+                else {
+                    runner = runner->next;
+                }
+            }
+
+            curr = curr->next;
+        }
+    }
+
+    std::vector<T> to_vector() const {
+        std::vector<T> vec;
+        std::shared_ptr<Node<T>> curr = head;
+        while (curr != nullptr) {
+            vec.push_back(curr->value);
+            curr = curr->next;
+        }
+        return vec;
+    }
+};
+
+template <typename T>
+void check_equal(const std::vector<T>& lhs, const std::vector<T>& rhs) {
+    assert(std::equal(lhs.begin(), lhs.end(), rhs.begin()));
+}
+
+template <typename T>
+void check_equal(const List<T>& lhs, const std::vector<T>& rhs) {
+    check_equal(lhs.to_vector(), rhs);
+}
+
+int main() {
+    {
+        List<int> lst;
+        lst.remove_dups();
+        check_equal(lst, {});
+    }
+    {
+        List<int> lst;
+        lst.append(1);
+        lst.remove_dups();
+        check_equal(lst, { 1 });
+    }
+    {
+        List<int> lst;
+        lst.append(1);
+        lst.append(2);
+        lst.append(3);
+        lst.append(2);
+        lst.append(4);
+        lst.remove_dups();
+        check_equal(lst, { 1, 2, 3, 4 });
+    }
+    {
+        List<int> lst;
+        lst.append(5);
+        lst.append(5);
+        lst.append(5);
+        lst.append(5);
+        lst.append(5);
+        lst.remove_dups();
+        check_equal(lst, { 5 });
+    }
+}
